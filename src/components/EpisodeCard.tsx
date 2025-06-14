5cad4ef2035a2a16633c11efab9013a14abbfa6a
jeremias <jeremiaskoch.92@gmail.com>
2025-06-13 23:21:05 -0300

    test

--- src/components/EpisodeCard.tsx
+++ src/components/EpisodeCard.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React from 'react';
+import { Link } from 'react-router';
 interface Episode {
-  id: number
-  name: string
-  episode: string
-  air_date: string
+  id: number;
+  name: string;
+  episode: string;
+  air_date: string;
 }
 
 interface Props {
-  episode: Episode
+  episode: Episode;
 }
 
 const EpisodeCard = ({ episode }: Props) => (
   <div className="border rounded p-4 bg-white shadow">
     <h2 className="font-bold text-lg">{episode.name}</h2>
-    <p className="text-sm">{episode.episode} - {episode.air_date}</p>
+    <p className="text-sm">
+      {episode.episode} - {episode.air_date}
+    </p>
     <Link
       to={`/episode/${episode.id}`}
       className="text-blue-500 underline mt-2 inline-block"
@@ -22,6 +24,6 @@ const EpisodeCard = ({ episode }: Props) => (
       View Details
     </Link>
   </div>
-)
+);
 
-export default EpisodeCard
+export default EpisodeCard;
